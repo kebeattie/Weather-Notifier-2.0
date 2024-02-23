efc5fbc076383ae5c3a5db17b52c051b0c23a7ed
Kyle Ellison-Beattie <kebeattie98@hotmail.com>
2024-02-23 15:02:20 +0000

    Adding API Func

--- Weather_Notif.py
+++ Weather_Notif.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import re
 from bs4 import BeautifulSoup
 from win10toast import ToastNotifier
@@ -7,13 +8,47 @@ from win10toast import ToastNotifier
 
 n = ToastNotifier()
 
+userInput = input("Enter your location: ")
+
+response_API = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/datatype/6620/xml/3840?res=3hourly&key=60071ca7-b9e5-4686-92bc-4d405064b6ca")
+
+sitelist = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=60071ca7-b9e5-4686-92bc-4d405064b6ca")
+
+list = sitelist.json()
+
+#for i in list:
+   # for j in i:
+       # if j == "name":
+           #print (x)
+            
+#print (list)
+
+locations = (list["Locations"] ["Location"])
+#print (locations)
+
+name = ([sub["name"] for sub in locations])
+
+#if userInput == name:
+    
+id = ([sub["name"] for sub in locations])
+
+print (id)
+
+#for i in locations:
+    #for j in i:
+        #print (j)
+
+print (response_API.status_code)
+
+
 def getweather(url):
     r = requests.get(url)
     return r.text
 
-htmldata = getweather("https://www.metoffice.gov.uk/weather/forecast/gcmzggpxq#?date=2024-03-25")
+htmldata = getweather("https://www.metoffice.gov.uk/weather/forecast/gcmzggpxq#?date=2024-02-23")
 
 soup = BeautifulSoup (htmldata, 'html.parser')
+
 
 #print (soup.prettify())
 
