8f6eb0ba576bf7dbde4c7d999cbc58ce3d0873ee
Kyle Ellison-Beattie <kebeattie98@hotmail.com>
2024-02-23 16:50:57 +0000

    App now connects to API and get temp and precip for Area that user inputs

--- Weather_Notif.py
+++ Weather_Notif.py
@@ -5,58 +5,43 @@ from bs4 import BeautifulSoup
 from win10toast import ToastNotifier
 
 
-
 n = ToastNotifier()
 
 userInput = input("Enter your location: ")
 
-sitelist = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=60071ca7-b9e5-4686-92bc-4d405064b6ca")
+sitelist = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=60071ca7-b9e5-4686-92bc-4d405064b6ca") # Retrieves JSON with all Uk location IDs
 
 list = sitelist.json()
        
 locations = (list["Locations"] ["Location"])
 
-name = ([sub["name"] for sub in locations])
+name = ([sub["name"] for sub in locations]) # Creates list with all location names
   
-id = ([sub["id"] for sub in locations])
+id = ([sub["id"] for sub in locations]) # Creates list with all location IDs
 
-no = 0
+no = 0 # Counter so that we can match location name with id from two lists
 
-for i in name:
-    no = no +1
+for i in name: # Checks that the user input matches a name in the locations list
+    no = no +1 
     if i == userInput:
         print (i)
         break
 
-locationID = id[no-1]
+locationID = id[no-1] # The counter we created earlier lets us match the location name with id. id is needed to pass into the URL for the API request
 
-response_API = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/" + locationID + "?res=3hourly&key=60071ca7-b9e5-4686-92bc-4d405064b6ca")
+response_API = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" + locationID + "?res=3hourly&key=60071ca7-b9e5-4686-92bc-4d405064b6ca") # Requests weather data for user specified location
 
-print(locationID)
-
-print (response_API.status_code)
+weatherData = response_API.json()
 
 
-def getweather(url):
-    r = requests.get(url)
-    return r.text
+data = (weatherData["SiteRep"]["DV"]["Location"]["Period"][4]["Rep"]) # Createslist of relevant weather data from JSON
 
-htmldata = getweather("https://www.metoffice.gov.uk/weather/forecast/gcmzggpxq#?date=2024-02-23")
+t = ([sub["T"] for sub in data]) # Extracts all temp values and stores in list
 
-soup = BeautifulSoup (htmldata, 'html.parser')
+p = ([sub["Pp"] for sub in data]) # Extracts all precip values and stores in list
 
+#print (response_API.status_code)
 
-#print (soup.prettify())
+result = "Current Temp = " + t[3] + "°C\n" + "Chance of Rain = " + p[3] + "%" + " in " + userInput # Puts all releveant information into string to be passed into notification
 
-high_temp = str(soup.find("span", class_="tab-temp-high"))
-
-low_temp = str(soup.find("span", class_="tab-temp-low"))
-
-max_temp = re.findall("[0-9]+", high_temp)
-
-min_temp = re.findall("[0-9]+", low_temp)
-
-
-result = "High temp = " + max_temp[0] + "°C" + " Low temp = " + min_temp[0] + "°C" + " in Liverpool"
-
-n.show_toast("Live Weather Update", result, duration = 10)
+n.show_toast("Live Weather Update", result, duration = 10) # Sends notification
