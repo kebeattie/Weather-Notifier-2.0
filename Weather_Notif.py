6f832e5779fce5cce70c46b28f66b8cf3520b10b
Kyle Ellison-Beattie <kebeattie98@hotmail.com>
2024-02-24 11:20:50 +0000

    Changing API to OpenWeather

--- Weather_Notif.py
+++ Weather_Notif.py
@@ -29,22 +29,23 @@ for i in name: # Checks that the user input matches a name in the locations list
 
 locationID = id[no-1] # The counter we created earlier lets us match the location name with id. id is needed to pass into the URL for the API request
 
-response_API = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" + locationID + "?res=3hourly&key=60071ca7-b9e5-4686-92bc-4d405064b6ca") # Requests weather data for user specified location
+response_API = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=2d05883852e6679dea8af8488a408f09") # Requests weather data for user specified location
 
 weatherData = response_API.json()
+print (weatherData)
 
 
-data = (weatherData["SiteRep"]["DV"]["Location"]["Period"][4]["Rep"]) # Createslist of relevant weather data from JSON
+#data = (weatherData["SiteRep"]["DV"]["Location"]["Period"][4]["Rep"]) # Createslist of relevant weather data from JSON
 
-t = ([sub["T"] for sub in data]) # Extracts all temp values and stores in list
+#t = ([sub["T"] for sub in data]) # Extracts all temp values and stores in list
 
-p = ([sub["Pp"] for sub in data]) # Extracts all precip values and stores in list
+#p = ([sub["Pp"] for sub in data]) # Extracts all precip values and stores in list
 
 #print (response_API.status_code)
 
-print (t)
-print (p)
+#print (t)
+#print (p)
 
-result = "Current Temp = " + t[3] + "°C\n" + "Chance of Rain = " + p[3] + "%" + " in " + userInput # Puts all releveant information into string to be passed into notification
+#result = "Current Temp = " + t[3] + "°C\n" + "Chance of Rain = " + p[3] + "%" + " in " + userInput # Puts all releveant information into string to be passed into notification
 
-n.show_toast("Live Weather Update", result, duration = 10) # Sends notification
+#n.show_toast("Live Weather Update", result, duration = 10) # Sends notification
