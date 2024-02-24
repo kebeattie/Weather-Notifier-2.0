0881c66c09b4cc9a184c332fd4f9bc3744fa6ae6
Kyle Ellison-Beattie <kebeattie98@hotmail.com>
2024-02-24 15:15:19 +0000

    OpenWeather API now functional

--- Weather_Notif.py
+++ Weather_Notif.py
@@ -1,51 +1,30 @@
 import requests
 import json
 import re
-from bs4 import BeautifulSoup
 from win10toast import ToastNotifier
-
 
 n = ToastNotifier()
 
 userInput = input("Enter your location: ")
 
-sitelist = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=60071ca7-b9e5-4686-92bc-4d405064b6ca") # Retrieves JSON with all Uk location IDs
+sitelist = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&limit=1&limit=1&appid=2d05883852e6679dea8af8488a408f09") # Retrieves JSON site lat and lon
 
-list = sitelist.json()
+list = sitelist.json() # Puts data from API call into json object
+
+lat = str((list[0]["lat"]))
+lon = str((list[0]["lon"])) #extracts lat and lon from json. To be passed into next API call
        
-locations = (list["Locations"] ["Location"])
+response_API = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat=" + lat +"&lon=" + lon +"&units=metric&exclude=minutely,hourly,daily,alerts&appid=2d05883852e6679dea8af8488a408f09") # Requests weather data for user specified location
 
-name = ([sub["name"] for sub in locations]) # Creates list with all location names
-  
-id = ([sub["id"] for sub in locations]) # Creates list with all location IDs
+weatherData = response_API.json() # Puts data from API call into json object
 
-no = 0 # Counter so that we can match location name with id from two lists
+currentTemp = (weatherData["current"]["temp"]) # Extracts current temp from json
+print (currentTemp)
 
-for i in name: # Checks that the user input matches a name in the locations list
-    no = no +1 
-    if i.upper() == userInput.upper():
-        print (i)
-        break
-
-locationID = id[no-1] # The counter we created earlier lets us match the location name with id. id is needed to pass into the URL for the API request
-
-response_API = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=2d05883852e6679dea8af8488a408f09") # Requests weather data for user specified location
-
-weatherData = response_API.json()
-print (weatherData)
+condition = (weatherData["current"]["weather"][0]["description"]) # Extracts current condtion from json
+print (condition)
 
 
-#data = (weatherData["SiteRep"]["DV"]["Location"]["Period"][4]["Rep"]) # Createslist of relevant weather data from JSON
+result = "Current Temp = " + str(currentTemp) + "°C\n" + "Condition = " + condition + " in " + userInput # Puts all releveant information into string to be passed into notification
 
-#t = ([sub["T"] for sub in data]) # Extracts all temp values and stores in list
-
-#p = ([sub["Pp"] for sub in data]) # Extracts all precip values and stores in list
-
-#print (response_API.status_code)
-
-#print (t)
-#print (p)
-
-#result = "Current Temp = " + t[3] + "°C\n" + "Chance of Rain = " + p[3] + "%" + " in " + userInput # Puts all releveant information into string to be passed into notification
-
-#n.show_toast("Live Weather Update", result, duration = 10) # Sends notification
+n.show_toast("Live Weather Update", result, duration = 10) # Sends notification
