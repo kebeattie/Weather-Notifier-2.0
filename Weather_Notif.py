127ebf3a52ad15e9c636a718746c2b4e62667398
Kyle Ellison-Beattie <kebeattie98@hotmail.com>
2024-02-24 17:05:11 +0000

    Added requirements and hidden API key

--- Weather_Notif.py
+++ Weather_Notif.py
@@ -1,6 +1,10 @@
 import requests
 import json
 from win10toast import ToastNotifier
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 n = ToastNotifier()
 lat = None
@@ -9,7 +13,7 @@ lon = None
 while (lat == None or lon == None):
     userInput = input("Enter your location: ")
 
-    sitelist = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput.title() + "&limit=1&limit=1&appid=2d05883852e6679dea8af8488a408f09") # Retrieves JSON site lat and lon
+    sitelist = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput.title() + "&limit=1&limit=1&appid="+os.getenv("API_KEY")) # Retrieves JSON site lat and lon
 
     list = sitelist.json() # Puts data from API call into json object
     
@@ -19,16 +23,13 @@ while (lat == None or lon == None):
     except:
         print ("\nLocation not found\n")
        
-response_API = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat=" + lat +"&lon=" + lon +"&units=metric&exclude=minutely,hourly,daily,alerts&appid=2d05883852e6679dea8af8488a408f09") # Requests weather data for user specified location
+response_API = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat=" + lat +"&lon=" + lon +"&units=metric&exclude=minutely,hourly,daily,alerts&appid="+os.getenv("API_KEY")) # Requests weather data for user specified location
 
 weatherData = response_API.json() # Puts data from API call into json object
 
 currentTemp = (weatherData["current"]["temp"]) # Extracts current temp from json
-print (currentTemp)
 
 condition = (weatherData["current"]["weather"][0]["description"]) # Extracts current condtion from json
-print (condition)
-
 
 result = "Current Temp = " + str(currentTemp) + "°C\n" + "Condition = " + condition.title() # Puts all releveant information into string to be passed into notification
 
