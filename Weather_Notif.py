98e48925433b4fff52cc8bcd67b03aed1f3ec542
Kyle Ellison-Beattie <kebeattie98@hotmail.com>
2024-02-24 15:31:46 +0000

    Added error handling for bad location input

--- Weather_Notif.py
+++ Weather_Notif.py
@@ -4,15 +4,21 @@ import re
 from win10toast import ToastNotifier
 
 n = ToastNotifier()
+lat = None
+lon = None
 
-userInput = input("Enter your location: ")
+while (lat == None or lon == None):
+    userInput = input("Enter your location: ")
 
-sitelist = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&limit=1&limit=1&appid=2d05883852e6679dea8af8488a408f09") # Retrieves JSON site lat and lon
+    sitelist = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput.title() + "&limit=1&limit=1&appid=2d05883852e6679dea8af8488a408f09") # Retrieves JSON site lat and lon
 
-list = sitelist.json() # Puts data from API call into json object
-
-lat = str((list[0]["lat"]))
-lon = str((list[0]["lon"])) #extracts lat and lon from json. To be passed into next API call
+    list = sitelist.json() # Puts data from API call into json object
+    
+    try:
+        lat = str((list[0]["lat"]))
+        lon = str((list[0]["lon"])) #extracts lat and lon from json. To be passed into next API call
+    except:
+        print ("\nLocation not found\n")
        
 response_API = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat=" + lat +"&lon=" + lon +"&units=metric&exclude=minutely,hourly,daily,alerts&appid=2d05883852e6679dea8af8488a408f09") # Requests weather data for user specified location
 
@@ -25,6 +31,6 @@ condition = (weatherData["current"]["weather"][0]["description"]) # Extracts cur
 print (condition)
 
 
-result = "Current Temp = " + str(currentTemp) + "°C\n" + "Condition = " + condition + " in " + userInput # Puts all releveant information into string to be passed into notification
+result = "Current Temp = " + str(currentTemp) + "°C\n" + "Condition = " + condition + " in " + userInput.title() # Puts all releveant information into string to be passed into notification
 
 n.show_toast("Live Weather Update", result, duration = 10) # Sends notification
