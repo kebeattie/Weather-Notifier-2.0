e49bc306e2c024a33f7897f136da0ce94ea2c5d1
Kyle Ellison-Beattie <kebeattie98@hotmail.com>
2024-02-26 15:22:57 +0000

    Fully converted to web app and handled errors

--- Weather_Notif.py
+++ Weather_Notif.py
@@ -7,40 +7,50 @@ import os
 load_dotenv()
 
 #n = ToastNotifier()
-
+status = 0
 
 def getWeather (city = "Liverpool"):
     lat = None
     lon = None
 
-    while (lat == None or lon == None):
+    #
         
 
-        sitelist = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=" + city.title() + "&limit=1&limit=1&appid="+os.getenv("API_KEY")) # Retrieves JSON site lat and lon
+    sitelist = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=" + city.title() + "&limit=1&limit=1&appid="+os.getenv("API_KEY")) # Retrieves JSON site lat and lon
+        
+       
+    
+    list = sitelist.json() # Puts data from API call into json object
 
-        list = sitelist.json() # Puts data from API call into json object
-
-        try:
-            lat = str((list[0]["lat"]))
-            lon = str((list[0]["lon"])) #extracts lat and lon from json. To be passed into next API call
-        except:
-            print ("\nLocation not found\n")
-            city = input("Enter your location: ")
+    try:
+        lat = str((list[0]["lat"]))
+        lon = str((list[0]["lon"])) #extracts lat and lon from json. To be passed into next API call
+    except: 
+        
+         print ("\nLocation not found\n")
+#        city = input("Enter your location: ")
+            
 
     response_API = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat=" + lat +"&lon=" + lon +"&units=metric&exclude=minutely,hourly,daily,alerts&appid="+os.getenv("API_KEY")) # Requests weather data for user specified location
-
+    #status = sitelist.status_code
+        
+    
     weatherData = response_API.json() # Puts data from API call into json object
     
     return weatherData
 
-
-
-
+def getStatus():
+    return status
+    
+status = getStatus()
+    
 if __name__ == "__main__":
     
     city = input("Enter your location: ")
 
     data = getWeather(city)
+    
+    
 
     #currentTemp = (data["current"]["temp"]) # Extracts current temp from json
 
--- server.py
+++ server.py
@@ -1,25 +1,35 @@
 from flask import Flask, render_template, request
-from Weather_Notif import getWeather
+from Weather_Notif import *
 from waitress import serve
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
+
 def index():
     return render_template('index.html')
 
+@app.route('/error')
+
+def error():
+    return render_template('error.html')
+
 @app.route('/weather')
+
 def weather():
-    city = request.args.get("city")
-    weather_data = getWeather(city)
-    return render_template (
+    try:
+        city = request.args.get("city").title()
+        weather_data = getWeather(city)
+        return render_template (
         "weather.html",
         title = city,
         cond = (weather_data["current"]["weather"][0]["description"]),
         temp = (weather_data["current"]["temp"]),
-        
-    )
-    
+
+        )
+    except:
+        return render_template('/error.html')
+
 
 if __name__ == "__main__":
     serve(app, host = "0.0.0.0", port = 8000)
